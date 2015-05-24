7329312b1ea282f2296f6dfc5774c3321a076a58
Lujing Cen <bobbyluig@gmail.com>
2015-05-24 09:04:16 -0700

    sample extension

--- src/asteroidGame/Chaser.java
+++ src/asteroidGame/Chaser.java
@@ -1,5 +1,12 @@
 package asteroidGame;
 
-public class Chaser {
-
+public class Chaser extends Asteroid {
+	public Chaser(double x, double y, double radius, double minVelocity, double maxVelocity, int hitsLeft, int numSplit) {
+		super(x, y, radius, minVelocity, maxVelocity, hitsLeft, numSplit);
+	}
+	
+	@Override
+	public void move() {
+		// How the Chaser moves. 
+	}
 }
