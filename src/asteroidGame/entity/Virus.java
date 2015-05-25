9611a7888b3c2e4d85009dde0a2a988cbba8bda4
AWildMudkip <drakex565@gmail.com>
2015-05-24 22:57:29 -0500

    Virus Methods To Be Written

--- src/asteroidGame/entity/Virus.java
+++ src/asteroidGame/entity/Virus.java
@@ -1,6 +1,30 @@
 package asteroidGame.entity;
 
-public class Virus {
-	
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Random;
 
+public class Virus extends Enemy
+{
+    int numProjectiles;
+    public Virus(double x, double y, double radius, double minVelocity, double maxVelocity, int hitsLeft, int numSplit, Color color) {
+		super(x, y, radius, minVelocity, maxVelocity, hitsLeft, color);
+	
+    }
+    public void move()
+    {
+    
+    }
+    public void draw(Graphics g)
+    {
+        
+    }
+    public void virusExplode()
+    {
+        
+    }
+    public static Color randomColor()
+    {
+        
+    }        
 }
