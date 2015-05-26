08bb1450fd711d52a57d0fbdf15dbc619feaea3d
AWildMudkip <drakex565@gmail.com>
2015-05-25 20:39:50 -0500

    sonic forever :P

--- src/asteroidGame/entity/powerup/SpeedUp.java
+++ src/asteroidGame/entity/powerup/SpeedUp.java
@@ -1,13 +1,14 @@
 package asteroidGame.entity.powerup;
 
 import asteroidGame.World;
+import java.awt.Graphics;
 
 public class SpeedUp extends Powerup {
 
 	public SpeedUp(double x, double y) {
 		super(x, y, "speedup.png");
 		
-		life = 5 * World.fps;
+		life = -1;
 	}
-	
+        
 }
